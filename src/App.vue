38f001b622c1de2cd89a9f7e49c6ebf3cf0f7d61
wangxing901120 <1647824858@qq.com>
2023-02-15 16:49:44 +0800

    feat: test

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <p>test-3000</p>
+    <p>test-3000-test</p>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
