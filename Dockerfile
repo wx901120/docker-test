c99d37975a471d20da1e85d32a42202b2a6d4599
wangxing901120 <1647824858@qq.com>
2023-02-23 07:35:52 +0800

    chore: del base of vite.config.ts file

--- Dockerfile
+++ Dockerfile
@@ -16,6 +16,5 @@
 
 # 本地测试
 FROM nginx
-COPY . .
 COPY ./dist /usr/share/nginx/html
 EXPOSE 80
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/docker-test/',
+  // base: '/docker-test/',部署到云服务器上不需要这个
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
