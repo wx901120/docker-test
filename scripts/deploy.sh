27cb9bca694c754e187996c93037bfa1c4d06584
wangxing901120 <1647824858@qq.com>
2023-02-23 07:48:01 +0800

    chore: edit deploy.sh && ci.yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,5 +4,5 @@
 echo -e '----docker pull----'
 docker pull wx9527/vite:1.0.0 # 从Docker Hub库里面下载镜像
 echo -e '---- create container and run ----'
-docker run --rm -d -p 80:80 --name vite-container wx9527/vite:1.0.0
+docker run --rm -d -p 80:80 --name vite-container wx9527/vite:1.0.1
 echo -e '----deploy success ----'
