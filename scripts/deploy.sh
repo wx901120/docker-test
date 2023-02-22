99756ebdf8b7bc99a6c4af0d601671b234634f6a
wangxing901120 <1647824858@qq.com>
2023-02-23 07:02:11 +0800

    chore: edit deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-echo "abc,$1,$2"
+# echo -e '----docker Login----'
+# docker login -u=$1 -p=$2
+echo -e '----docker pull----'
+docker pull wx9527/vite:1.0.0 # 从Docker Hub库里面下载镜像
+echo -e '---- create container and run ----'
+docker run --rm -d -p 80:80 --name vite-container wx9527/vite:1.0.0
+echo -e '----deploy success ----'
