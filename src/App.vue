314b958e1a25dfc145c319204bf730d6e9266f23
wangxing901120 <1647824858@qq.com>
2023-02-23 08:12:14 +0800

    chore: edit deploy.sh && ci.yml

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Docker test By vue3+vite" />
+      <HelloWorld msg="vue3+vite" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
